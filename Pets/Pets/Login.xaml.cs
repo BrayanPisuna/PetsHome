dddc3e29eb1bf0762fa9e35bf6f521817a943fea
master <master@SMLAB108PC21_23>
2023-07-18 21:17:33 -0500

    Agregar archivos de proyecto.

---
+++ Pets/Pets/Login.xaml.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Pets
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        string url;
+        string secret;
+
+        public Login(string url, string secret)
+        {
+            InitializeComponent();
+            this.url = url;
+            this.secret = secret;
+
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+
+
+            try
+            {
+                WebClient cliente = new WebClient();
+                cliente.Headers[HttpRequestHeader.ContentType] = "application/json";
+
+                Usuario user = new Usuario(lblUser.Text, lblPass.Text);
+
+                string json = JsonConvert.SerializeObject(user);
+
+                App.token = cliente.UploadString(this.url + "login", "POST", json).Replace("Bearer ", String.Empty);
+
+                //var token = new  JwtSecurityToken(App.token);
+                //new JwtSecurityToken(token);
+
+
+                //var mensaje = "Dato ingresado con exito";
+                //var tokenHandler = new JwtSecurityTokenHandler();
+                //SecurityToken securityToken;
+                //var securityKey = new SymmetricSecurityKey(Encoding.Default.GetBytes(this.secret));
+                //var expiration = DateTime.UtcNow.AddHours(1);
+                //var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+                //var validationParameters = new TokenValidationParameters()
+                //{
+                //    ValidateLifetime = true,
+                //    //ValidateIssuerSigningKey = true,
+                //    ValidAudience = "mvc.manual",
+                //    IssuerSigningKey = securityKey
+                //};
+
+
+                //var parametros = tokenHandler.ValidateToken(App.token, validationParameters, out securityToken);
+
+                //var a=parametros;
+                //DisplayAlert("ALERTA",parametros, "OK");
+
+                //DependencyService.Get<Mensaje>().longAlert(mensaje);
+
+                Navigation.PushAsync(new Validacion());
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Ok", ex.Message, "OK");
+                //DependencyService.Get<Mensaje>().longAlert(ex.Message);
+            }
+
+
+
+        }
+        private void ButtonCrear_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Registro());
+        }
+    }
+}
---
+++ Pets/Pets/Validacion.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Pets
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Validacion : ContentPage
+	{
+		public Validacion ()
+		{
+			InitializeComponent ();
+		}
+
+        private async void ButtonDoc_Clicked(object sender, EventArgs e)
+        {
+			var opcion = new StoreCameraMediaOptions
+			{
+				SaveToAlbum = true,
+				Name = lblNombre + "_" + DateTime.Now
+			};
+			var foto = CrossMedia.Current.TakePhotoAsync(opcion);
+			MiImagen.Source = ImageSource.FromStream(() =>
+			{
+				var stream = foto.Result.GetStream() ;
+				foto.Dispose();
+				return stream;
+			});
+
+        }
+    }
+}
